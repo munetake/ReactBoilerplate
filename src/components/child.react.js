6cde29f31ca1a05449e1f21d28fd603d1f18b8c4
munetake <kaseymunetake@gmail.com>
2016-06-15 18:49:57 -0700

    Fixed styling

--- src/components/child.react.js
+++ src/components/child.react.js
@@ -27,15 +27,15 @@ export default class Child extends Component {
       <div className>
         <div className="row">
           <label className="col-md-2">Name: </label>
-          <input type="text" className="col-md-4" name="name" onChange={this._handleChange}/>
+          <input type="text" className="col-md-4 wayLeft" name="name" placeholder="Name..." onChange={this._handleChange}/>
         </div>
         <div className="row">
           <label className="col-md-2">Address: </label>
-          <input type="text" className="col-md-4" name="address" onChange={this._handleChange}/><br/>
+          <input type="text" className="col-md-4 wayLeft" name="address" placeholder="Address..." onChange={this._handleChange}/><br/>
         </div>
         <div className="row">
           <label className="col-md-2">Favorite Quote: </label>
-          <input type="text" className="col-md-4" name="quote" onChange={this._handleChange}/><br/>
+          <input type="text" className="col-md-4 wayLeft" name="quote" placeholder="Favorite Quote..." onChange={this._handleChange}/><br/>
         </div>
         <button type="button" className="btn btn-primary" onClick={this._handleClick}>Submit</button>
       </div>
