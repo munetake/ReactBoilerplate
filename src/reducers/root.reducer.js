7ccd484e782cc3a4858e70e4909ec83ff75a8f20
munetake <kaseymunetake@gmail.com>
2016-06-24 14:44:07 -0700

    Deleted unnecessary function

--- src/reducers/root.reducer.js
+++ src/reducers/root.reducer.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux';
 import ChangeChild from "./change_child.reducer";
 import ChangeDisplay from "./change_display.reducer";
-function test(){
-  return {book: 1};
-}
+
 
 const rootReducer = combineReducers({
   list: ChangeChild
